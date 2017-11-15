08890f1ff33c8205bf623fbc4f1d0e7b5b3aa4d7
John Ypma (HAS) <j.ypma@has.nl>
2017-11-15 09:52:38 +0100

    Initiele versie met OSM achtergrondkaart

---
+++ kaart.js
@@ -0,0 +1,20 @@
+$(document).ready(function()
+{
+     
+    var mapOptions = 
+    {
+        zoomControl: true,
+        zoom: 8,
+        center: [52, 5]
+    };
+
+
+    var kaart = L.map("kaart", mapOptions);
+    
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 12, attribution: osmAttrib});
+    
+    kaart.addLayer(osm);
+    
+ });
