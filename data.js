389a477e416a72bcaa2226dfbbb9c00dc1ed98a7
johnyhas61 <j.ypma@has.nl>
2017-11-26 14:34:24 +0100

    Code na afloop van 1e les

---
+++ data.js
@@ -0,0 +1,172 @@
+var mijnJSON = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "Naam": "Van Tienhovenschool",
+            "Straat": "Simonskerkestraat",
+            "plaats": "Amsterdam",
+            "soort": "school"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [4.794359921129134, 52.355180209376755]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Cartesiuslyceum",
+            "Straat": "Piet Mondriaanstraat",
+            "plaats": "Amsterdam",
+            "soort": "school"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [4.838221634271728, 52.365829615696555]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "HTS Wiltzanghlaan",
+            "Straat": "Wiltzanghlaan",
+            "plaats": "Amsterdam",
+            "soort": "school"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [4.845405836145207, 52.381789618394556]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "CvI",
+            "Straat": "Croeselaan",
+            "plaats": "Utrecht",
+            "soort": "werk"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.111101180548478, 52.08225371212357]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "NS RIB",
+            "Straat": "Moreelsepark",
+            "plaats": "Utrecht",
+            "soort": "werk"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.113677400640989, 52.08897558001418]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Koning Willem I College",
+            "Straat": "Onderwijsboulevard 3",
+            "plaats": "s-Hertogenbosch",
+            "soort": "werk"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.287604439082256, 51.68895651257958]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "HAS Hogeschool",
+            "Straat": "Onderwijsboulevard 221",
+            "plaats": "s-Hertogenbosch",
+            "soort": "werk"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.285966477084886, 51.68642972802311]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Thuis",
+            "Straat": "Johan Hofmanstraat 39",
+            "plaats": "Amsterdam",
+            "soort": "wonen"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [4.79840430202274, 52.3529938910656]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Thuis",
+            "Straat": "Chopinplein",
+            "plaats": "Culemborg",
+            "soort": "wonen"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.238231582949922, 51.95241145624071]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Thuis",
+            "Straat": "Braspenninglaan 16",
+            "plaats": "s-Hertogenbosch",
+            "soort": "wonen"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.28661438290185, 51.73335579927367]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Voetbal",
+            "Straat": "Boelelaan",
+            "plaats": "Amsterdam",
+            "soort": "sport"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [4.873334510940729, 52.334700581589445]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Badminton",
+            "Straat": "Jan van Galenstraat",
+            "plaats": "Amsterdam",
+            "soort": "sport"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [4.8502792044774115, 52.373973196916594]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Badminton",
+            "Straat": "Marathonloop",
+            "plaats": "s-Hertogenbosch",
+            "soort": "sport"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.316227884191618, 51.72228182739087]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Naam": "Volleybal",
+            "Straat": "Braspenninglaan 140",
+            "plaats": "s-Hertogenbosch",
+            "soort": "sport"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [5.284173418161954, 51.73275123055027]
+        }
+    }]
+};
--- kaart.js
+++ kaart.js
@@ -19,4 +19,62 @@ $(document).ready(function () {
 
     kaart.addLayer(osm);
 
+    var schoolIcon = new L.Icon({
+        iconUrl: 'school.png',
+        iconAnchor: [25, 50],
+        popupAnchor: [0, -50]
+    });
+
+    var werkIcon = new L.Icon({
+        iconUrl: 'werk.png',
+        iconAnchor: [25, 50],
+        popupAnchor: [0, -50]
+    });
+
+    var woonIcon = new L.Icon({
+        iconUrl: 'thuis.png',
+        iconAnchor: [25, 50],
+        popupAnchor: [0, -50]
+    });
+
+    var sportIcon = new L.Icon({
+        iconUrl: 'sport.png',
+        iconAnchor: [25, 50],
+        popupAnchor: [0, -50]
+    });
+
+    var jsonLaag = new L.GeoJSON(mijnJSON, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.Naam);
+        },
+        pointToLayer: function (feature, latLng) {
+            if (feature.properties.soort == "school") {
+                return new L.Marker(latLng, {
+                    icon: schoolIcon
+                });
+
+            }
+            if (feature.properties.soort == "werk") {
+                return new L.Marker(latLng, {
+                    icon: werkIcon
+                });
+
+            }
+            if (feature.properties.soort == "wonen") {
+                return new L.Marker(latLng, {
+                    icon: woonIcon
+                });
+
+            }
+            if (feature.properties.soort == "sport") {
+                return new L.Marker(latLng, {
+                    icon: sportIcon
+                });
+
+            }
+        }
+    });
+
+    kaart.addLayer(jsonLaag);
+
 });
