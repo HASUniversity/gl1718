a208fd7fa23f40edffa2b2833c7f0d5023de04ef
John Ypma (HAS) <j.ypma@has.nl>
2017-11-30 16:09:26 +0100

    Code aan het eind van les 2 met WMS en lagenswitcher

--- kaart.js
+++ kaart.js
@@ -6,42 +6,45 @@ $(document).ready(function () {
         center: [52, 5]
     };
 
-
     var kaart = L.map("kaart", mapOptions);
 
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
     var osm = new L.TileLayer(osmUrl, {
         minZoom: 2,
         maxZoom: 20,
         attribution: osmAttrib
     });
-
     kaart.addLayer(osm);
+    
+    
+    var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+    kaart.addLayer(googleSat);
 
     var schoolIcon = new L.Icon({
         iconUrl: 'school.png',
         iconAnchor: [25, 50],
         popupAnchor: [0, -50]
     });
-
     var werkIcon = new L.Icon({
         iconUrl: 'werk.png',
         iconAnchor: [25, 50],
         popupAnchor: [0, -50]
     });
-
     var woonIcon = new L.Icon({
         iconUrl: 'thuis.png',
         iconAnchor: [25, 50],
         popupAnchor: [0, -50]
     });
-
     var sportIcon = new L.Icon({
         iconUrl: 'sport.png',
         iconAnchor: [25, 50],
         popupAnchor: [0, -50]
     });
+
 
     var jsonLaag = new L.GeoJSON(mijnJSON, {
         onEachFeature: function (feature, layer) {
@@ -52,29 +55,44 @@ $(document).ready(function () {
                 return new L.Marker(latLng, {
                     icon: schoolIcon
                 });
-
             }
             if (feature.properties.soort == "werk") {
                 return new L.Marker(latLng, {
                     icon: werkIcon
                 });
-
             }
             if (feature.properties.soort == "wonen") {
                 return new L.Marker(latLng, {
                     icon: woonIcon
                 });
-
             }
             if (feature.properties.soort == "sport") {
                 return new L.Marker(latLng, {
                     icon: sportIcon
                 });
-
             }
         }
     });
-
     kaart.addLayer(jsonLaag);
 
+    var provincieWMS = new L.TileLayer.WMS('http://gmd.has.nl:8080/geoserver/geolab/wms', {
+        layers: 'geolab:nl-prov',
+        transparent: true,
+        format: 'image/png'
+    });
+    kaart.addLayer(provincieWMS);
+
+    var achtergronden = {
+        'Open streetmap': osm,
+        'Satelliet': googleSat
+    };
+    
+    var overlayLagen = {
+        'markers': jsonLaag,
+        'provincies': provincieWMS
+    };
+    
+    var lagenSwitcher = new L.Control.Layers(achtergronden, overlayLagen);
+    kaart.addControl(lagenSwitcher);
+    
 });
